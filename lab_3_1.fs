046ac8fa102cdad257b922470ebf8aff1f56a003
jeiwyy <jeiwy1333@gmail.com>
2026-03-12 09:57:20 +0500

    add lab_3_1.fs

---
+++ lab_3_1.fs
@@ -0,0 +1,19 @@
+// Получить список из длин строк, содержащихся в исходной последовательности
+open System
+
+let rec readSequence () = seq {
+    printf "Введите добавляемое значение (ctrl+d(z) - конец): "
+    let addVal = Console.ReadLine()
+    if addVal <> null then
+        yield addVal
+        yield! readSequence()
+}
+
+[<EntryPoint>]
+let main args = 
+    printfn "Создание последовательности: "
+    let ourSeq = readSequence()
+    let resSeq = ourSeq |> Seq.map String.length
+    printfn "Исходная последовательность: %A" (ourSeq |> Seq.toList)
+    printfn "Список длин строк: %A" (resSeq |> Seq.toList)
+    0
